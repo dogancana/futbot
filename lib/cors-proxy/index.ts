0dd2057e4152f653ce0ebf5c95fd81a051e9f3cf
Hans-Peter Buniat <hpbuniat@googlemail.com>
2019-10-13 15:09:56 +0200

    (fix) use tmpdir to write session & static-items

--- lib/cors-proxy/index.ts
+++ lib/cors-proxy/index.ts
@@ -1,17 +1,17 @@
-const CorsAnywhere = require('cors-anywhere')
+const CorsAnywhere = require('cors-anywhere');
 
 const host = process.env.CORS_PROXY_HOST || '0.0.0.0';
 const port = process.env.CORS_PROXY_PORT || 9998;
-let started = false
+let started = false;
 
 export function startCorsProxy() {
   if (!started) {
-    started = true
+    started = true;
     CorsAnywhere.createServer({
       originWhitelist: [], // Allow all origins
       requireHeader: ['origin', 'x-requested-with'],
       removeHeaders: []
-    }).listen(port, host, function() {
+    }).listen(port, host, function () {
       console.log('Running CORS Anywhere on ' + host + ':' + port);
     })
   }
