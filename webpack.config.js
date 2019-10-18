9c1356b017f22071515b3659a1e38946aa6fd541
Dogancan Arabaci <dogancan.arabaci@gmail.com>
2019-10-18 11:34:22 +0200

    Github actions added to build two packages

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './lib/app.ts',
+  mode: 'production',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'server.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['ts-loader']
+      }
+    ]
+  }
+};
