770d2241f8b7a2a85f824ecd464c9b17661f0d7d
Dogancan Arabaci <dogancan.arabaci@gmail.com>
2019-11-04 18:24:00 +0100

    feat(cicd): adjusted for monorepo

---
+++ package.sh
@@ -0,0 +1,14 @@
+rm -rf futbot.zip
+mkdir futbot
+
+cp packages/server/.env.local futbot/.env
+cp -R packages/extension/build futbot/extension
+cp -R packages/server/dist/* futbot
+chmod +x futbot/server/server-macos
+chmod +x futbot/server/server-win.exe
+
+apt-get update
+apt-get install zip
+zip -r futbot.zip futbot
+
+rm -rf futbot
